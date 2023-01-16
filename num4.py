9962301716743327462daf07f7f001806e927d6d
daeyubkang <yub102447@gmail.com>
2023-01-16 23:04:12 +0900

    01/16-2

--- num4.py
+++ num4.py
@@ -19,6 +19,9 @@ for i in range(2, n+1):
     for j in range(2, n+1):
         sum[i][j] = a[i][j] + sum[i][j-1] + sum[i-1][j] - sum[i-1][j-1]
 
+print(a)
+print(sum)
+
 for i in range(quizNo):
     x1,y1,x2,y2 = map(int,input().split())
     print(sum[x2][y2] + sum[x1-1][y1-1] - sum[x2][y1-1] - sum[x1-1][y2])
